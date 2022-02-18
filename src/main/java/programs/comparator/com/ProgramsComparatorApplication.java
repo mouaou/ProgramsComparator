77ab8b87504c4a573076ef5c293c13fb6547e841
Youssef M <youssef.mouaou@gmail.com>
2022-02-18 15:59:05 +0100

    creation du projet

---
+++ src/main/java/programs/comparator/com/ProgramsComparatorApplication.java
@@ -0,0 +1,13 @@
+package programs.comparator.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramsComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramsComparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programs/comparator/com/ProgramsComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package programs.comparator.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramsComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
