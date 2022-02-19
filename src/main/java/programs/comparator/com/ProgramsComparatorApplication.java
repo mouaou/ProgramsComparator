a8a1953f0e05b928848cb2e0b979d62c55414fe8
EL MAHI Zakariae <zakariae.elmahi@soprabanking.com>
2022-02-19 02:48:39 +0100

    preapare list new & list old

--- src/main/java/programs/comparator/com/ProgramsComparatorApplication.java
+++ src/main/java/programs/comparator/com/ProgramsComparatorApplication.java
@@ -5,15 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import programs.comparator.com.reader.DirectoryReader;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class ProgramsComparatorApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ProgramsComparatorApplication.class, args);
 		DirectoryReader directoryReader = new DirectoryReader();
-		String dirLocation = "D:/sopra/programs";
 
-		directoryReader.buildProgramsListClassic(dirLocation);
+		Path s = Paths.get("C:\\Users\\zelmahi\\Desktop\\Patch_SGMA\\PROD_SGMA\\TestProject");
+		
+		directoryReader.buildListPrograms(s);
 
 
 	}
--- src/main/java/programs/comparator/com/reader/DirectoryReader.java
+++ src/main/java/programs/comparator/com/reader/DirectoryReader.java
@@ -39,8 +39,11 @@ public class DirectoryReader {
 
     }
     //build list of new and old programs
-    public static void buildListPrograms(Path path) throws IOException {
+    public void buildListPrograms(Path path) throws IOException {
         List<Path> listDirectories = getListDirectories(path);
+        List<Program> listNewPrograms = new ArrayList<>();
+        List<Program> listOldPrograms = new ArrayList<>();
+
         listDirectories.forEach(dir->{
             try {
                 List<Path> listFiles = getListFiles(dir);
@@ -48,48 +51,24 @@ public class DirectoryReader {
                     Program program = new Program();
                     program.setName(file.getFileName().toString());
                     program.setSize(file.toFile().length());
+                    String md5 = org.apache.commons.codec.digest.DigestUtils.md5Hex(new FileInputStream(file.toFile()));
+                    program.setMd5Code(md5);
+                    if(dir.endsWith("NewPrograms")){
+                        program.setNewVersion(true);
+                        listNewPrograms.add(program);
+                    }else if(dir.endsWith("OldPrograms")){
+                        program.setNewVersion(false);
+                        listOldPrograms.add(program);
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+        System.out.println("*****list new programs********");
+        listNewPrograms.forEach(program1 -> System.out.println(""+program1.toString()+""));
+        System.out.println("*****list old programs********");
+        listOldPrograms.forEach(program1 -> System.out.println(""+program1.toString()+""));
     }
-
-
-    public void buildProgramsListClassic(String dirLocation){
-        List<Program> programsList = new ArrayList<>();
-
-        List<File> files = null;
-
-        {
-            try {
-                files = Files.list(Paths.get(dirLocation))
-                        .map(Path::toFile)
-                        .collect(Collectors.toList());
-                for(File file : files){
-                    if(file.isDirectory()){
-                        buildProgramsListClassic(file.getPath());
-                    }
-                    else{
-                        Program program = new Program();
-                        program.setName(file.getName());
-                        program.setSize(file.length());
-                        String md5 = org.apache.commons.codec.digest.DigestUtils.md5Hex(new FileInputStream(file));
-                        program.setDateCreation(file.lastModified());
-                        program.setMd5Code(md5);
-                        programsList.add(program);
-                    }
-
-                }
-            } catch (IOException  e) {
-                e.printStackTrace();
-            }
-
-            programsList.forEach(program1 -> System.out.println("*"+program1.toString()+"*"));
-        }
-    }
-
-
-
 
 }
