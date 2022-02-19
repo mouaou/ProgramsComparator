9fb471b8c535a3a3d8b7de3182832236e2d164b4
Youssef M <youssef.mouaou@gmail.com>
2022-02-19 02:48:36 +0100

    build list old and new programs v2

--- src/main/java/programs/comparator/com/ProgramsComparatorApplication.java
+++ src/main/java/programs/comparator/com/ProgramsComparatorApplication.java
@@ -5,15 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import programs.comparator.com.reader.DirectoryReader;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class ProgramsComparatorApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ProgramsComparatorApplication.class, args);
 		DirectoryReader directoryReader = new DirectoryReader();
-		String dirLocation = "D:/sopra/programs";
+		Path path = Paths.get("D:/sopra/programs");
+		directoryReader.buildListPrograms(path);
 
-		directoryReader.buildProgramsListClassic(dirLocation);
 
 
 	}
